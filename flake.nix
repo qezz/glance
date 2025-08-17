4d26c85d83e7318be146468ba3786dc914c158d3
Sergey Mishin <sergei.a.mishin@gmail.com>
2025-08-18 00:11:07 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Glance - a Gleam source code parser, in Gleam";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            erlang
+            gleam
+            rebar3
+            erlang-ls
+          ];
+        };
+      }
+    );
+}
